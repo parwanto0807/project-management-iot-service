0d671cd70408e8866accece818b1e87dc6868af3
parwanto0807 <parwanto0807@gmail.com>
2025-01-04 06:14:35 +0700

    Change Background

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -10,7 +10,7 @@ const font = Poppins({
 
 export default function Home() {
   return (
-    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-b from-sky-400 to-blue-800">
+    <main className="flex h-full flex-col items-center justify-center bg-gradient-to-b from-sky-500 to-blue-950">
 <div className="space-y-6 text-center">
         <h1
           className={cn(
